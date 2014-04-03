bcf01c4872294ae1d4077ca5891be2420b07aa4b
Arne <arne@ArneNB.(none)>
2014-04-04 01:14:11 +0200

    initial commit

---
+++ install_lib.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp ./build/devel/lib/libyoubot_moveit_controller_manager.so /opt/ros/hydro/lib/
+sudo cp ./build/devel/lib/pkgconfig/youbot_moveit_controller_manager.pc /opt/ros/hydro/lib/pkgconfig/
